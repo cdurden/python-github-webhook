096ffb279e04abddd56919a08c2759ff3c0e19f8
Chris Durden <cdurden@gmail.com>
2020-03-27 10:12:57 -0500

    logging

--- algebra742_webhooks.py
+++ algebra742_webhooks.py
@@ -13,11 +13,13 @@ def hello_world():
 
 @webhook.hook()        # Defines a handler for the 'push' event
 def on_push(data):
+    print("Got push on repository '{:s}'".format(data['repository']['name']))
     process = subprocess.Popen(['git','pull'], cwd=os.path.join(os.environ['WSGI_APPS_PATH'], data['repository']['name']), stdout=PIPE, stderr=PIPE)
     stdoutput, stderroutput = process.communicate()
     process = subprocess.Popen(['git','pull'], cwd=os.path.join(os.environ['WSGI_APPS_PATH'], 'dev', data['repository']['name']), stdout=PIPE, stderr=PIPE)
     stdoutput, stderroutput = process.communicate()
     if data['repository']['name'] in ['md','slides','tasks']:
+        print("updating teaching_assets")
         process = subprocess.Popen(['git','submodule','foreach','git','pull','origin','master'], cwd=os.path.join(os.environ['WSGI_APPS_PATH'], 'teaching_assets'), stdout=PIPE, stderr=PIPE)
         stdoutput, stderroutput = process.communicate()
     if data['repository']['name'] in ['algebra742']:
@@ -27,7 +29,7 @@ def on_push(data):
         stdoutput, stderroutput = process.communicate()
     #process = subprocess.Popen(["sudo", "pm2", "restart", "server"], stdout=PIPE, stderr=PIPE)
     #stdoutput, stderroutput = process.communicate()
-    print("Got push with: {0}".format(data))
+    #print("Got push with: {0}".format(data))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
